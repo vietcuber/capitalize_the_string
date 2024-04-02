be90072246181fde99a5c343ebefdbbf5d2df593
vietcuber <nvietanh28@gmail.com>
2024-04-02 19:42:12 +0700

    Second commit
    Change the code to be more optimized

--- main.cpp
+++ main.cpp
@@ -6,9 +6,8 @@ int main() {
 	printf("Enter a string (less than 100 characters): ");
 	fgets(string, 100, stdin);
 	for (int i = 0; string[i] != '\0'; i++) {
-		if (('A' <= string[i] && string[i] <= 'Z') || string[i] == ' ' || string[i] == '\n')
-			continue;
-		string[i] -= 32;
+		if ('a' <= string[i] && string[i] <= 'z')
+			string[i] -= 32;
 	}
 	printf("The string entered after capitalization is: %s", string);
 	return 0;
